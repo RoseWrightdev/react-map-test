9829d81224276576d56cbb528da325300bcff165
Rose Wright <rosesophiewright@gmail.com>
2023-09-12 20:46:15 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+const turtles = [
+  {
+      name: "Leonardo",
+      nickName: "Leo",
+      weapon: "Katana",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/en/e/ed/Leonardo_%28Teenage_Mutant_Ninja_Turtles%29.jpg"
+  },
+  {
+      name: "Donatello",
+      nickName: "Don",
+      weapon: "Bo staff",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/en/5/5a/Donatello_%28Teenage_Mutant_Ninja_Turtles%29.jpg"
+  },
+  {
+      name: "Michelangelo",
+      nickName: "Mikey",
+      weapon: "Nunchucks",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/en/f/f3/Michelangelo_%28Teenage_Mutant_Ninja_Turtles%29.jpg"
+  },
+  {
+      name: "Raphael",
+      nickName: "Raph",
+      weapon: "Sai",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/en/7/72/Raphael_%28Teenage_Mutant_Ninja_Tutles%29.jpg"
+  }
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+  {turtles.map((turtle, index) =>
+      <div key={turtle.name + "-" + index}>
+        <h1>{turtle.name} ({turtle.nickName})</h1>
+        <p>Weapon of choice: {turtle.weapon}</p>
+        <img src={turtle.imgUrl} alt={`${turtle.name}`} width="200"/>
+        <hr/>
+      </div>
+      )}
+   </div>
   );
 }
 
